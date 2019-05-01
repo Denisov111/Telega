8dc30b1d98bc3882229e631eb7094f7d94b3567a
М <@DESKTOP-ARVP0H8>
2019-05-02 03:24:23 +0700

    dsgfb

--- Telega/MainWindow.xaml.cs
+++ Telega/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using TLSharp.Core;
+using System.Net.Sockets;
+using xNet.Net;
+using System.IO;
 
 namespace Telega
 {
@@ -21,8 +24,9 @@ namespace Telega
     /// </summary>
     public partial class MainWindow : Window
     {
-        int apiId = 0;
-        string apiHash = "";
+        int apiId = 753326;
+        string apiHash = "78a12e22bd20d62c3b5759e3e60cb483";
+        bool isCodeReady;
 
         public MainWindow()
         {
@@ -36,13 +40,55 @@ namespace Telega
 
         async private void Go()
         {
-            var client = new TelegramClient(apiId, apiHash);
-            await client.ConnectAsync();
+            string proxy = "5.101.82.125:8000:Bj9BvB:fGtkgw";
 
-            var hash = await client.SendCodeRequestAsync("<user_number>");
-            var code = "<code_from_telegram>"; // you can change code in debugger
+            TcpClient TcpHandler(string address, int port)
+            {
+                string[] split = proxy.Split(':');
+                var socksProxy = new HttpProxyClient(split[0], int.Parse(split[1]), split[2], split[3]);
+                var tcpClient = socksProxy.CreateConnection(address, port);
+                return tcpClient;
+            }
 
-            var user = await client.MakeAuthAsync("<user_number>", hash, code);
+            if (File.Exists("session.dat"))
+            {
+                var client = new TelegramClient(apiId, apiHash, sessionUserId:"session", handler: TcpHandler);
+                await client.ConnectAsync();
+                FileSessionStore store = new FileSessionStore();
+                TLSharp.Core.Session s = null; s = store.Load("session");
+                //var hash = await client.SendCodeRequestAsync("79029828756");
+                //string code = await GetCode();
+
+                //var user = await client.MakeAuthAsync("79029828756", hash, code);
+                var userByPhoneId = await client.SearchUserAsync("79029828756");
+            }
+            else
+            {
+                var client = new TelegramClient(apiId, apiHash, handler: TcpHandler);
+                await client.ConnectAsync();
+
+                var hash = await client.SendCodeRequestAsync("79029828756");
+                string code = await GetCode();
+
+                var user = await client.MakeAuthAsync("79029828756", hash, code);
+            }
+
+            
+            
+        }
+
+        async private Task<string> GetCode()
+        {
+            while(!isCodeReady)
+            {
+                await Task.Delay(50);
+            }
+            return codeTextBox.Text;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            isCodeReady = true;
         }
     }
 }
